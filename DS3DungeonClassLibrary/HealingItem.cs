c7809110c007d26dd5f3e9843ac2cf5afd1544cb
Sean Slicer <87379816+SeanSlicer@users.noreply.github.com>
2022-08-04 01:31:44 -0500

    Healing item class added

---
+++ DS3DungeonClassLibrary/HealingItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS3DungeonClassLibrary
+{
+    public class HealingItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int HealingAmount { get; set; }
+        
+        public HealingItem(string name, string description, int healingAmount)
+        {
+            Name = name;
+            Description = description ;
+            HealingAmount = healingAmount;
+            
+        }//end FQCTOR
+        public override string ToString()
+        {
+            return string.Format($"");
+        }
+
+    }
+}
