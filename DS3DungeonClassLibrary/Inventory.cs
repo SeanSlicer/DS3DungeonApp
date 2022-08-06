c5c39b7de3e75492f1101f0b9ce026998f2d7ea5
Sean Slicer <87379816+SeanSlicer@users.noreply.github.com>
2022-08-05 22:06:24 -0500

    Inventory class created

---
+++ DS3DungeonClassLibrary/Inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DS3DungeonClassLibrary;
+
+namespace DS3DungeonClassLibrary
+{
+    internal class Inventory
+    {
+        List<Weapon> inventoryWeapons = new List<Weapon>();
+        List<HealingItem> healingItems = new List<HealingItem>();
+    }
+}
