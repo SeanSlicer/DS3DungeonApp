6a9d48eefcd8ed393fd57e95ab09d38ed4beefe6
Sean Slicer <87379816+SeanSlicer@users.noreply.github.com>
2022-08-08 00:23:09 -0500

    Initialized Healing Items

---
+++ DS3DungeonClassLibrary/HealingItems.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DS3DungeonClassLibrary
+{
+    public static class Healingitems
+    {
+        //Healing Items
+
+       public static readonly HealingItem minorPotion = new HealingItem("Minor Potion", "A potion to restore a small amount of health", 25);
+    }
+}
