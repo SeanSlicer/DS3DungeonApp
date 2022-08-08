0d79d80dd04e6d8ed3f020a2da7750b3ea563c9e
Sean Slicer <87379816+SeanSlicer@users.noreply.github.com>
2022-08-08 00:28:16 -0500

    Healing Items finished for now

--- DS3DungeonClassLibrary/HealingItems.cs
+++ DS3DungeonClassLibrary/HealingItems.cs
@@ -6,5 +6,6 @@ namespace DS3DungeonClassLibrary
         //Healing Items
 
        public static readonly HealingItem minorPotion = new HealingItem("Minor Potion", "A potion to restore a small amount of health", 25);
+        public static readonly HealingItem estusFlask = new HealingItem("Estus Flash", "A potion of great healing", 75);
     }
 }
